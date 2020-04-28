fb0b646d9557201ac0c080075fbe9fb0631727bb
OrellanaCesar <orellanacesar.1992@gmail.com>
2020-04-28 00:32:58 -0300

    creacion de modelo y migraciones

--- src/models/DetallePedido.py
+++ src/models/DetallePedido.py
@@ -1,8 +1,6 @@
 
-
-
 from . import db
-
+from marshmallow import fields, Schema
 
 class DetallePedido(db.Model):
     __table_name__ = "detallepedido"
@@ -18,3 +16,33 @@ class DetallePedido(db.Model):
         self.detalle_cantidad = cantidad
         self.detalle_descripcion = descripcion
         self.detalle_total = total
+
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    
+    @staticmethod
+    def get_all():
+        return DetallePedido.query.all()
+
+
+    @staticmethod
+    def get_one_cliente(id):
+        return DetallePedido.query.get(id)
+
+
+class DetallePedidoSchema(Schema):
+    detallepedido_id = fields.Int(dumo_only=True)
+    empanadas_id = fields.Int(dumo_only=True)
+    pedido_id = fields.Int(dumo_only=True)
+    detalle_cantidad = fields.Int(required=True)
+    detalle_descripcion = fields.String(required=True)
+    detalle_total = fields.Float(required=True)
+   
