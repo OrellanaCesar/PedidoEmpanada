cadbbacb7877a2009b1964ca0b13dfaebb8ff6b3
OrellanaCesar <orellanacesar.1992@gmail.com>
2020-05-01 18:25:35 -0300

    angular 8 navbar

--- app-pedidos/src/app/components/navbar/navbar.component.ts
+++ app-pedidos/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
+
+import { Router, ActivatedRoute } from '@angular/router';
+import * as jQuery from 'jquery';
 
 @Component({
   selector: 'app-navbar',
@@ -6,11 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  
   constructor() { }
-
+  public title:string = 'App-Pedido';
+  public toogle:boolean = false;
   public app_name: string = 'App-Pedidos';
   ngOnInit(): void {
+   
+  }
+
+  showNavbar():void{
+    jQuery('#wrapper').toggleClass("toggled");
   }
 
 }
